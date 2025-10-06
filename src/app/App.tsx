6b74faabdb31071892dfb551649525ff30ab01b9
ekFrontDev <egors.cube@gmail.com>
2025-10-06 22:41:38 +0300

    создан первый виджет

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,22 +3,17 @@ import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { NavBar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главаня</Link>
-      <Link to={"/about"}>О сайте</Link>
+      
+      <NavBar />
       <AppRouter />
-      {/* <Suspense fallback={<div> Loading</div>}> */}
-      {/* <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes> */}
-      {/* <AppRouter /> */}
-      {/* </Suspense> */}
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
